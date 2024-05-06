fa3b1a23cce63e658ba23f4a4ca4269c10aaf746
Gitsagar1223 <connectsagar02@gmail.com>
2024-05-06 20:53:52 +0530

    useState hook implementation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from "react";
+import Registration from "./Registration";
+
+const App = () => {
+  return (
+    <div className="App">
+      App
+      <Registration />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Registration.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+const Registration = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(input);
+    setInput({
+      fullname: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <h1>Registration Form</h1>
+
+        <div className="form-input">
+          <label>Full Name: </label>
+          <input
+            type="text"
+            placeholder="Enter full name"
+            value={input.fullname}
+            onChange={handleChange}
+            name="fullname"
+          ></input>
+
+          <label>email: </label>
+          <input
+            type="text"
+            placeholder="domain@email.com"
+            value={input.email}
+            onChange={handleChange}
+            name="email"
+          ></input>
+
+          <label>Password: </label>
+          <input
+            type="text"
+            placeholder="******"
+            value={input.password}
+            onChange={handleChange}
+            name="password"
+          ></input>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
